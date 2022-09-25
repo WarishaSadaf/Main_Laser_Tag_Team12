7f9052419d46485ea45b176d77acafc836bb5e29
JoseBaules <42388209+JoseBaules@users.noreply.github.com>
2022-09-25 10:57:30 -0500

    Add files via upload

---
+++ src/main/java/com/example/project/Scene2Controller.java
@@ -0,0 +1,84 @@
+package com.example.project;
+import java.util.*;
+import java.lang.*;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Scene2Controller {
+
+    private Stage stage;
+
+    private Scene scene;
+
+    private Parent root;
+
+    @FXML
+    private Label welcomeText;
+
+    public Scene2Controller() {
+    }
+
+    @FXML
+
+
+
+    public void EditGame (ActionEvent event) throws IOException
+    {
+
+    }
+    public void StartGame (ActionEvent event) throws IOException
+    {
+
+    }
+    public void ViewGame (ActionEvent event) throws IOException
+    {
+
+    }
+    public void PreEnteredGames (ActionEvent event) throws IOException
+    {
+
+    }
+    public void FlickSync (ActionEvent event) throws IOException
+    {
+
+    }
+
+    public void ClearGame (ActionEvent event) throws IOException
+    {
+
+    }
+
+    public class Controller
+    {
+        public void move_up ()
+        {
+
+        }
+        public void move_down()
+        {
+
+        }
+        public void move_left ()
+        {
+
+        }
+        public void move_right()
+        {
+
+        }
+    }
+    public void closeGame(ActionEvent event) throws IOException
+    {
+        System.exit(0);
+
+    }
+
+}
