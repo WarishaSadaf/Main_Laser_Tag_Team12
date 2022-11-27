dd50534381a9bec5f1ebc54e159399ced4cb5811
JoseBaules <jvillarbaules@gmail.com>
2022-11-26 23:38:30 -0600

    Comments
    Co-Authored-By: WarishaSadaf <112606831+WarishaSadaf@users.noreply.github.com>
    Co-Authored-By: alison-nicole <109484898+alison-nicole@users.noreply.github.com>
    Co-Authored-By: Josine  <96550452+josi543@users.noreply.github.com>

--- src/main/java/com/example/project/HelloApplication.java
+++ src/main/java/com/example/project/HelloApplication.java
@@ -22,9 +22,7 @@ public class HelloApplication extends Application
     {
         Parent root = FXMLLoader.load(getClass().getResource("Scene2.fxml"));
         Scene scene = new Scene(root);
-
         stage.setScene(scene);
-        
         stage.show();
     }
 
